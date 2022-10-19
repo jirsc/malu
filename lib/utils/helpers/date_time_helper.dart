13e9758c739d3fe9980f16043bc2d9d2dff9e7d6
ji_desktop <jiroisaac7@gmail.com>
2022-10-19 11:41:34 +0800

    feat(datetime helper): added formatDateToString

--- lib/utils/helpers/date_time_helper.dart
+++ lib/utils/helpers/date_time_helper.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart';
+
 /// Find the first date of the week which contains the provided [dateTime].
 DateTime findFirstDateOfTheWeek({required DateTime dateTime}) {
   return dateTime.subtract(Duration(days: dateTime.weekday - 1));
@@ -6,4 +8,10 @@ DateTime findFirstDateOfTheWeek({required DateTime dateTime}) {
 /// Find last date of the week which contains provided [dateTime].
 DateTime findLastDateOfTheWeek({required DateTime dateTime}) {
   return dateTime.add(Duration(days: DateTime.daysPerWeek - dateTime.weekday));
+}
+
+/// Format [date] to string using provided [format].
+String formatDateToString({required DateTime date, required String format}) {
+  DateFormat formatter = DateFormat(format);
+  return formatter.format(date);
 }
