f70243cd3f784737a26bf6ef0a7e19f1c9737fa5
ji_desktop <jiroisaac7@gmail.com>
2022-10-19 11:43:56 +0800

    feat(meal plan): added meal and meal plan models

--- packages/authentication_repository/lib/authentication_repository.dart
+++ packages/authentication_repository/lib/authentication_repository.dart
@@ -1,4 +1,7 @@
 library authentication_repository;
 
 export 'src/authentication_repository.dart';
+
+export 'src/models/meal.dart';
+export 'src/models/meal_plan.dart';
 export 'src/models/user.dart';
