ee8a838e4d3ce779d68734f99a651420541c0a87
ji_desktop <jiroisaac7@gmail.com>
2022-10-19 11:40:48 +0800

    refactor: rearrange folder structure of plan.dart

--- lib/modules/plan/plan.dart
+++ lib/modules/plan/plan.dart
@@ -1,5 +1,7 @@
 export 'views/plan_screen.dart';
-export 'views/day_plan_widget.dart';
-export 'views/week_view_calendar_widget.dart';
+
+export 'widgets/day_plan_widget.dart';
+export 'widgets/plan_screen_skeleton.dart';
+export 'widgets/week_view_calendar_widget.dart';
 
 export 'bloc/plan_bloc.dart';
