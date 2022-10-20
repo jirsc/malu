b9387bf0429fb7645baab59286acf99810119e65
ji_desktop <jiroisaac7@gmail.com>
2022-10-20 12:00:13 +0800

    fix: add layout builder for device screen constraints

--- lib/widgets/randomizer_grid_widget.dart
+++ lib/widgets/randomizer_grid_widget.dart
@@ -6,7 +6,6 @@ import '../models/food.dart';
 import 'vertical_tile_card.dart';
 
 class RandomizerGrid extends StatefulWidget {
-
   const RandomizerGrid({
     Key? key,
     required this.foodList,
@@ -31,46 +30,50 @@ class _RandomizerGridState extends State<RandomizerGrid> {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      // Create a grid with 2 columns. If you change the scrollDirection to
-      // horizontal, this produces 2 rows.
-      crossAxisCount: 2,
-      children: List.generate(widget.listCount, (index) {
-        if (index == 0) {
-          return Card(
-            elevation: 0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: const Icon(
-                    FontAwesome4.play_circled,
-                    size: 40,
+    return LayoutBuilder(builder: (context, constraint) {
+      return GridView.count(
+        // Create a grid with 2 columns. If you change the scrollDirection to
+        // horizontal, this produces 2 rows.
+        crossAxisCount: constraint.maxWidth > 500 ? 3 : 2,
+        children: List.generate(widget.listCount, (index) {
+          if (index == 0) {
+            return Card(
+              elevation: 0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: const Icon(
+                      FontAwesome4.play_circled,
+                      size: 40,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        itemList = randomizedList();
+                      });
+                    },
                   ),
-                  onPressed: () {
-                    setState(() {
-                      itemList = randomizedList();
-                    });
-                  },
-                ),
-                const Text('Randomize'),
-              ],
-            ),
-          );
-        }
-        return buildVerticalTileCard(itemList, index);
-      }),
-    );
+                  const Text('Randomize'),
+                ],
+              ),
+            );
+          }
+          return buildVerticalTileCard(itemList, index);
+        }),
+      );
+    });
   }
 
   Widget buildVerticalTileCard(List<Food> itemList, int index) {
     List<String> mealType = ['', 'BREAKFAST', 'LUNCH', 'DINNER'];
     return VerticalTileCardWithIcon(
       imageUrl: itemList[index].imageUrl,
-      text1: itemList[index].name,
-      text2: mealType[index],
+      text1: mealType[index],
+      text2: itemList[index].name,
       icon: const Icon(FontAwesome4.edit),
-      onPressed: () {},
+      onPressed: () {
+        print('text to');
+      },
     );
   }
 
--- lib/widgets/vertical_tile_card_with_icon.dart
+++ lib/widgets/vertical_tile_card_with_icon.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 typedef _IconButtonCallback = dynamic Function();
 
 class VerticalTileCardWithIcon extends StatelessWidget {
-
   const VerticalTileCardWithIcon({
     Key? key,
     required this.imageUrl,
@@ -22,77 +21,70 @@ class VerticalTileCardWithIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 7.0),
-        color: Colors.white,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 140,
-                  width: 140,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.asset(
-                      imageUrl,
-                      fit: BoxFit.cover,
+      child: LayoutBuilder(builder: (context, constraints) {
+        return Container(
+          padding: const EdgeInsets.symmetric(horizontal: 7.0),
+          color: Colors.white,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 140,
+                    width: 140,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.asset(
+                        imageUrl,
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 7),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        text1,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                          fontSize: 12,
-                          //fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                      const SizedBox(height: 4),
-                      Text(
-                        text2,
-                        style: const TextStyle(
-                          fontSize: 12,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      /* Text(
-                              product.description,
+                  const SizedBox(height: 7),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              text1,
                               overflow: TextOverflow.ellipsis,
                               style: const TextStyle(
-                                fontSize: 11,
-                                color: Colors.grey,
+                                fontSize: 12,
+                                //fontWeight: FontWeight.bold,
+                                color: Colors.black,
                               ),
                             ),
-                            const SizedBox(height: 7),
-                            _buildIconText(
-                              Icons.star,
-                              Colors.orange[300]!,
-                              '${vendor.score}(${vendor.ratingCount}k)',
+                            const SizedBox(height: 4),
+                            Text(
+                              text2,
+                              style: const TextStyle(
+                                fontSize: 12,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                            const SizedBox(width: 10),
-                            _buildIconText(
-                              Icons.visibility,
-                              Colors.grey,
-                              '${vendor.weeklyOrderCount}K Orders this week',
-                            ), */
-                    ],
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            IconButton(onPressed: onPressed, icon: icon),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+                ],
+              ),
+            ],
+          ),
+        );
+      }),
     );
   }
 }
